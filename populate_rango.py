1e54a8b8bda5ffb81721d1e97d8a186f7b522630
Choyeon Hong <2993949h@student.gla.ac.uk>
2025-02-02 18:55:06 +0000

    MTV

--- populate_rango.py
+++ populate_rango.py
@@ -6,40 +6,49 @@ django.setup()
 from rango.models import Category, Page
 
 def populate():
-    # 각 카테고리별로 추가할 페이지 데이터를 정의
     python_pages = [
-        {'title': 'Official Python Tutorial', 'url': 'http://docs.python.org/3/tutorial/'},
-        {'title': 'How to Think like a Computer Scientist', 'url': 'http://www.greenteapress.com/thinkpython/'},
-        {'title': 'Learn Python in 10 Minutes', 'url': 'http://www.korokithakis.net/tutorials/python/'}
-    ]
+        {'title': 'Official Python Tutorial',
+         'url': 'http://docs.python.org/3/tutorial/',
+         'views': 1},
+        {'title': 'How to Think like a Computer Scientist',
+         'url': 'http://www.greenteapress.com/thinkpython/',
+         'views': 2},
+        {'title': 'Learn Python in 10 Minutes',
+         'url': 'http://www.korokithakis.net/tutorials/python/',
+         'views': 3}]
 
     django_pages = [
-        {'title': 'Official Django Tutorial', 'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-        {'title': 'Django Rocks', 'url': 'http://www.djangorocks.com/'},
-        {'title': 'How to Tango with Django', 'url': 'http://www.tangowithdjango.com/'}
-    ]
+        {'title': 'Official Django Tutorial',
+         'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+         'views': 4},
+        {'title': 'Django Rocks',
+         'url': 'http://www.djangorocks.com/',
+         'views': 5},
+        {'title': 'How to Tango with Django',
+         'url': 'http://www.tangowithdjango.com/',
+         'views': 6}]
 
     other_pages = [
-        {'title': 'Bottle', 'url': 'http://bottlepy.org/docs/dev/'},
-        {'title': 'Flask', 'url': 'http://flask.pocoo.org'}
-    ]
+        {'title': 'Bottle',
+         'url': 'http://bottlepy.org/docs/dev/',
+         'views': 7},
+        {'title': 'Flask',
+         'url': 'http://flask.pocoo.org',
+         'views': 8}]
 
-    # 카테고리와 해당 페이지 데이터를 저장하는 딕셔너리 생성
-    cats = {
-        'Python': {'pages': python_pages, 'views':128, 'likes':64},
-        'Django': {'pages': django_pages, 'views':64, 'likes':32},
-        'Other Frameworks': {'pages': other_pages, 'views':32, 'likes':16}
-    }
-    # 딕셔너리를 순회하며 카테고리와 페이지를 데이터베이스에 추가
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}}
+
     for cat, cat_data in cats.items():
-        c = add_cat(cat, cat_data['views'], cat_data['likes'])  # 카테고리 추가
+        c = add_cat(cat, cat_data.get('views', 0), cat_data.get('likes', 0))
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])  # 페이지 추가
+            add_page(c, p['title'], p['url'], p['views'])
 
-    # 결과 출력
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
+
 
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
@@ -48,6 +57,7 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
+
 def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
     c.views = views
@@ -55,7 +65,64 @@ def add_cat(name, views=0, likes=0):
     c.save()
     return c
 
-# 스크립트 실행
+
 if __name__ == '__main__':
     print('Starting Rango population script...')
     populate()
+
+
+
+# def populate():
+#     # 각 카테고리별로 추가할 페이지 데이터를 정의
+#     python_pages = [
+#         {'title': 'Official Python Tutorial', 'url': 'http://docs.python.org/3/tutorial/'},
+#         {'title': 'How to Think like a Computer Scientist', 'url': 'http://www.greenteapress.com/thinkpython/'},
+#         {'title': 'Learn Python in 10 Minutes', 'url': 'http://www.korokithakis.net/tutorials/python/'}
+#     ]
+
+#     django_pages = [
+#         {'title': 'Official Django Tutorial', 'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+#         {'title': 'Django Rocks', 'url': 'http://www.djangorocks.com/'},
+#         {'title': 'How to Tango with Django', 'url': 'http://www.tangowithdjango.com/'}
+#     ]
+
+#     other_pages = [
+#         {'title': 'Bottle', 'url': 'http://bottlepy.org/docs/dev/'},
+#         {'title': 'Flask', 'url': 'http://flask.pocoo.org'}
+#     ]
+
+#     # 카테고리와 해당 페이지 데이터를 저장하는 딕셔너리 생성
+#     cats = {
+#         'Python': {'pages': python_pages, 'views':128, 'likes':64},
+#         'Django': {'pages': django_pages, 'views':64, 'likes':32},
+#         'Other Frameworks': {'pages': other_pages, 'views':32, 'likes':16}
+#     }
+#     # 딕셔너리를 순회하며 카테고리와 페이지를 데이터베이스에 추가
+#     for cat, cat_data in cats.items():
+#         c = add_cat(cat, cat_data['views'], cat_data['likes'])  # 카테고리 추가
+#         for p in cat_data['pages']:
+#             add_page(c, p['title'], p['url'])  # 페이지 추가
+
+#     # 결과 출력
+#     for c in Category.objects.all():
+#         for p in Page.objects.filter(category=c):
+#             print(f'- {c}: {p}')
+
+# def add_page(cat, title, url, views=0):
+#     p = Page.objects.get_or_create(category=cat, title=title)[0]
+#     p.url = url
+#     p.views = views
+#     p.save()
+#     return p
+
+# def add_cat(name, views=0, likes=0):
+#     c = Category.objects.get_or_create(name=name)[0]
+#     c.views = views
+#     c.likes = likes
+#     c.save()
+#     return c
+
+# # 스크립트 실행
+# if __name__ == '__main__':
+#     print('Starting Rango population script...')
+#     populate()
--- rango/models.py
+++ rango/models.py
@@ -1,14 +1,26 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+#every time the category name changes, the slug will also change 
+#– the save() method is always called when creating or updating an instance of a Django model.
+
 
 class Category(models.Model):
 
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-    
+    slug = models.SlugField(unique=True)
+
+#*args = positional arguments (as a tuple).
+#**kwargs = keyword arguments (as a dictionary).
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = 'categories'
+        
 
     def __str__(self):
         return self.name
